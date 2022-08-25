fff31e79f3ee3668c6bd8752fcc40774dfe1ade8
Sekiguchi.Naoya <sekiguchi@bizlink.co.jp>
2022-08-25 10:52:03 +0900

    設定見直し。説明追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,32 @@
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue2';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-import { defineConfig } from 'vite';
+// plugin説明
+//   @vitejs/plugin-vue2 ⇒ vue2 vite 利用の為の plugin
+//   vite-tsconfig-paths ⇒ ソースコードのalias解決(import hoge from '@/path/to/hoge' の "@" の解決）。vite.config.ts で resolve.alias を指定する方法もあるが、tsconfig.json の管理に統一する。
 
 export default defineConfig({
   plugins: [vue(), tsconfigPaths()],
+  // ライブラリモードなので、publicディレクトリは無効（設定されているとビルド時に dist へコピーされる）
   publicDir: false,
   // css: {
   //   devSourcemap: false,
   // },
   build: {
+    // ライブラリモードでビルドする場合は、build.lib の設定を追加する
     lib: {
+      // ライブラリエントリポイント
       entry: './src/vue-data-binder.ts',
       formats: ['es', 'cjs', 'umd' /*, 'iife' */],
+      // ライブラリ名
       name: '@bizlink/vue-data-binder',
     },
     rollupOptions: {
-      // ライブラリにバンドルされるべきではない依存関係を
-      // 外部化するようにします
+      // ライブラリにバンドルされるべきではない依存関係を外部化する
       external: ['vue'],
       output: {
-        // 外部化された依存関係のために UMD のビルドで使用する
-        // グローバル変数を提供します
+        // 外部化された依存関係のために UMD のビルドで使用するグローバル変数を提供します
         globals: {
           vue: 'Vue',
         },

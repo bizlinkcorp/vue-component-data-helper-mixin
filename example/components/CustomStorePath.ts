bf7c8d1695df4bc1fea3004389cbe368492f88c4
Sekiguchi.Naoya <sekiguchi@bizlink.co.jp>
2022-09-02 00:25:53 +0900

    パッケージ公開に当たり、説明しづらいサンプルは除外

--- example/components/CustomStorePath.ts
+++
@@ -1,9 +0,0 @@
-import { defineComponent } from 'vue';
-import { StorePath } from 'vue-data-binder';
-import CustomStorePathMixin from '../mixins/CustomStorePathMixin';
-
-export default defineComponent({
-  name: 'StorePath',
-  extends: StorePath,
-  mixins: [CustomStorePathMixin],
-});
--- example/store/ViewState.ts
+++
@@ -1,7 +0,0 @@
-import { ViewStateTree } from 'vue-data-binder';
-export interface AppViewState {
-  disabled?: boolean;
-  readonly?: boolean;
-}
-
-export type AppViewStateTree = ViewStateTree<AppViewState>;
